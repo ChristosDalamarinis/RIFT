da172b4732a9fb29d60306696dc9f7330b1ef6f8
Christos Dalamarinis <dalamarinischristos@gmail.com>
2026-02-06 22:18:25 +0100

    adjested the window and added documentaton in line 10 about how to use the loop in case the refresh rate cannot be automatically measured

--- Code for Gabors/rfr.py
+++ Code for Gabors/rfr.py
@@ -1,13 +1,13 @@
 from psychopy import visual
 
-win = visual.Window([1710, 1107], fullscr=False)
+win = visual.Window([1200, 900], fullscr=False)
 
 # Try to measure
 measured_rate = win.getActualFrameRate(nIdentical=60, nMaxFrames=120)
 
 if measured_rate is None:
     print("❌ Auto-detection FAILED - using fallback")
-    refresh_rate = 60.0
+    refresh_rate = 60.0 # Choose the refresh rate of the monitor if it cannot be measured
 else:
     print(f"✓ Auto-detected: {measured_rate:.2f} Hz")
     refresh_rate = measured_rate
